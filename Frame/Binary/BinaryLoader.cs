713234c89caaa1045fd40d60134a35377e4c4a94
MuteLovestone <mutelovestone@gmail.com>
2016-07-29 20:00:16 -0600

    Update to Previous State

---
+++ Frame/Binary/BinaryLoader.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+namespace DarkFold.Frame.Core.Binary
+{
+	/// <summary>
+	/// Binary loader.
+	/// </summary>
+	public class BinaryLoader:IDisposable
+	{
+		#region Fields
+		/// <summary>
+		/// The reader.
+		/// </summary>
+		private BinaryReader _reader;
+		#endregion
+		#region Constuctor
+		/// <summary>
+		/// Initializes a new instance of the <see cref="DarkFold.Frame.Core.BinaryLoader"/> class.
+		/// </summary>
+		/// <param name="path">Path.</param>
+		public BinaryLoader (string path)
+		{
+			_reader = new BinaryReader (File.OpenRead (path));
+		}
+		#endregion
+		#region Overrides
+		/// <summary>
+		/// Dispose this instance.
+		/// </summary>
+		public void dispose()
+		{
+			_reader.Dispose ();
+		}
+		#endregion
+		#region Load Methods
+		/// <summary>
+		/// Gets the byte.
+		/// </summary>
+		/// <returns>The byte.</returns>
+		public byte GetByte()
+		{
+			return _reader.ReadByte ();
+		}
+		/// <summary>
+		/// Gets the int.
+		/// </summary>
+		/// <returns>The int.</returns>
+		public int GetInt()
+		{
+			return _reader.ReadInt32 ();
+		}
+		/// <summary>
+		/// Gets the long.
+		/// </summary>
+		/// <returns>The long.</returns>
+		public Int64 GetLong()
+		{
+			return _reader.ReadInt64 ();
+		}
+		/// <summary>
+		/// Gets the string.
+		/// </summary>
+		/// <returns>The string.</returns>
+		public string GetString()
+		{
+			return _reader.ReadString ();
+		}
+		/// <summary>
+		/// Gets the bool.
+		/// </summary>
+		/// <returns><c>true</c>, if bool was gotten, <c>false</c> otherwise.</returns>
+		public bool GetBool()
+		{
+			return _reader.ReadBoolean ();
+		}
+		#endregion
+	}
+}
+
---
+++ Frame/Binary/BinarySaver.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+
+namespace DarkFold.Frame.Core.Binary
+{
+	/// <summary>
+	/// Binary saver.
+	/// </summary>
+	public class BinarySaver:IDisposable
+	{
+		#region Fields
+		/// <summary>
+		/// The writer.
+		/// </summary>
+		private BinaryWriter _writer;
+		#endregion
+		#region Constuctor
+		/// <summary>
+		/// Initializes a new instance of the <see cref="DarkFold.Frame.Core.BinarySaver"/> class.
+		/// </summary>
+		/// <param name="path">Path.</param>
+		public BinarySaver (string path)
+		{
+			_writer = new BinaryWriter (File.OpenWrite (path));
+		}
+		#endregion
+		#region Overrides
+		/// <summary>
+		/// Releases all resource used by the <see cref="DarkFold.Frame.Core.BinarySaver"/> object.
+		/// </summary>
+		/// <remarks>Call <see cref="Dispose"/> when you are finished using the <see cref="DarkFold.Frame.Core.BinarySaver"/>. The
+		/// <see cref="Dispose"/> method leaves the <see cref="DarkFold.Frame.Core.BinarySaver"/> in an unusable state. After
+		/// calling <see cref="Dispose"/>, you must release all references to the
+		/// <see cref="DarkFold.Frame.Core.BinarySaver"/> so the garbage collector can reclaim the memory that the
+		/// <see cref="DarkFold.Frame.Core.BinarySaver"/> was occupying.</remarks>
+		public void Dispose()
+		{
+			_writer.Dispose ();
+		}
+		#endregion
+		#region Write Methods
+		/// <summary>
+		/// Write the specified val.
+		/// </summary>
+		/// <param name="val">Value.</param>
+		public void Write(int val)
+		{
+			_writer.Write (val);
+		}
+		/// <summary>
+		/// Write the specified val.
+		/// </summary>
+		/// <param name="val">Value.</param>
+		public void Write(byte val)
+		{
+			_writer.Write (val);
+		}
+		/// <summary>
+		/// Write the specified val.
+		/// </summary>
+		/// <param name="val">Value.</param>
+		public void Write(string val)
+		{
+			_writer.Write (val);
+		}
+		/// <summary>
+		/// Write the specified val.
+		/// </summary>
+		/// <param name="val">Value.</param>
+		public void Write(Int64 val)
+		{
+			_writer.Write (val);
+		}
+		/// <summary>
+		/// Write the specified val.
+		/// </summary>
+		/// <param name="val">If set to <c>true</c> value.</param>
+		public void Write(bool val)
+		{
+			_writer.Write (val);
+		}
+		#endregion
+	}
+}
+
